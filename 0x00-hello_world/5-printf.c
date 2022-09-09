23db2128d2442a523b5375122e90d3e5267a609f
sackey619 <hsackey619thuglove@gmail.com>
2022-09-08 21:23:18 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main-Entry
+ *
+ *Return: 0 after printing the function
+ */
+
+int main(void)
+{
+  printf("with proper grammer, but the outcome is a piece of art,");
+  return (0);
+}
