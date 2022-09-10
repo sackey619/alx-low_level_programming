2ddc5db94d4b53092165d96e65b8d5729c9673f8
sackey619 <hsackey619thuglove@gmail.com>
2022-09-09 20:52:48 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,7 +7,8 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{  
+{
+  
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
@@ -22,6 +23,7 @@ printf("%d is zero\n", n);
 else
 {
 printf("%d is negative\n", n);
-}  
+}
+ 
 return (0);
 }
