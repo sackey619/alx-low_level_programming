7e99c6279f2eec7acdc748f5a0d0772464cb5f24
sackey619 <hsackey619thuglove@gmail.com>
2022-09-13 21:24:38 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,21 @@
-#include "holberton.h"
+#include"main.h"
+
 /**
- * main - Prints Holberton
+ * main - Entry point
+ *
+ * Description: prints _putchar using putchar prototype
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+  char str[] = "_putchar";
+  int ch;
 
-	return (0);
+  for (ch = 0; ch < 8; ++ch)
+    _putchar(str[ch]);
+  _putchar('\n');
+
+  return (0);
 }
