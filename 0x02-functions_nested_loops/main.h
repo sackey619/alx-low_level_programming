e8de1f6f86531e53125b1c753305f2f336c85016
sackey619 <hsackey619thuglove@gmail.com>
2022-09-13 21:09:45 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,33 +1,20 @@
-#ifndef FILE_HOLBERTON
-#define FILE_HOLBERTON
+#ifndef MAIN_H
+#define MAIN_H
 
-void print_alphabet(void);
-
-void print_alphabet_x10(void);
-
-void fibonacci(void);
+#include<stdio.h>
 
 int _putchar(char c);
-
+void print_alphabet(void);
+void print_alphabet_x10(void);
 int _islower(int c);
-
 int _isalpha(int c);
-
 int print_sign(int n);
-
 int _abs(int);
-
-void jack_bauer(void);
-
 int print_last_digit(int);
-
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
 void print_times_table(int n);
 
-void times_table(void);
-
-int add(int a, int b);
-
-void print_to_98(int n);
-
 #endif
-
