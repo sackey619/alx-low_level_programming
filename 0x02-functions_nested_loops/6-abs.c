c6a11fec058e5482147da54d3a2f5fd0f5ef0a10
sackey619 <hsackey619thuglove@gmail.com>
2022-09-21 19:49:19 -0700

    6-abcs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -11,16 +11,16 @@
 
 int _abs(int n)
 {
-  if (n < 0)
-    {
-      return (-n);
-    }
-  else if (n > 0)
-    {
-      return (n);
-    }
-  else
-    {
-      return (0);
-    }
+if (n < 0)
+{
+return (-n);
+}
+else if (n > 0)
+{
+return (n);
+}
+else
+{
+return (0);
+}
 }
