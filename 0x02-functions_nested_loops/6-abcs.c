2aaa3e37bfe5b3b7ff815947cea8b97843058a0a
sackey619 <hsackey619thuglove@gmail.com>
2022-09-21 19:38:00 -0700

    6-abcs.c

---
+++ 0x02-functions_nested_loops/6-abcs.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _abs - function that computes the absolute
+ *       value of an integer
+ *
+ * @n: takes in integer type input for function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int n)
+{
+  if(n < 0)
+    {
+      return (-n);
+    }
+  else if(n > 0)
+    {
+      return (n);
+    }
+  else
+    {
+      return (0);
+    }
+}
