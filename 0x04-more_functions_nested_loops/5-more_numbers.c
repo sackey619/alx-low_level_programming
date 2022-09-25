d1d9b91dca3b28caa2164563534533bc72eff888
sackey619 <hsackey619thuglove@gmail.com>
2022-09-25 06:07:41 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,18 +6,18 @@
 
 void more_numbers(void)
 {
-  int i, j;
+int i, j;
 
-  for (i = 0; i <= 9; i++)
-    {
-      for (j = 0; j <= 14; j++)
-	{
-	  if (j >= 10)
-	    {
-	      _putchar((j / 10) + '0');
-	    }
-	  _putchar((j % 10) + '0');
-	}
-      _putchar('\n');
-    }
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 14; j++)
+{
+if (j >= 10)
+{
+_putchar((j / 10) + '0');
+}
+_putchar((j % 10) + '0');
+}
+_putchar('\n');
+}
 }
